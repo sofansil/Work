e9911297ae746531f49f80782edcb65390192c51
sofansil <sofansil@naver.com>
2025-12-02 13:23:47 +0900

    클래스연습

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -2,12 +2,15 @@ strName = "Not Class Member"
 
 class DemoString:
     def __init__(self):
+        # 인스턴스 멤버 변수 초기화
         self.strName = "" 
     def set(self, msg):
         self.strName = msg
     def print(self):
-        print(str)
+        # 버그 발생!
+        print(self.strName)
 
+# 인스턴스 생성
 d = DemoString()
 d.set("First Message")
 d.print()
--- 상속01.py
+++ 상속01.py
@@ -1,3 +1,4 @@
+# 부모 클래스
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
@@ -6,17 +7,28 @@ class Person:
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
 
+# 자식 클래스
 class Student(Person):
+    # 덮어쓰기
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        # super() 함수
+        #self.name = name
+        #self.phoneNumber = phoneNumber
+        super().__init__(name, phoneNumber)
+
         self.subject = subject
         self.studentID = studentID
-
+    
+    # 덮어쓰기(재정의, override)
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(학과:{0}, 학번: {1})".format(self.subject, self.studentID))
 
 p = Person("전우치", "010-222-1234")
-s = Student("이순신", "010-111-1234", "컴공", "991122")
+s = Student("이순신", "010-111-1234", "인공지능", "230001")
 print(p.__dict__)
 print(s.__dict__)
 
+p.printInfo()
+s.printInfo()
 
